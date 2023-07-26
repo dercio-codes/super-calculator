285c5f96be944557be04d8eff473df39adc4bb62
Dercio Maduna <76833905+derciomaduna-codes@users.noreply.github.com>
2023-07-26 00:35:18 +0000

    Update 28 files

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,180 @@
-import '../styles/globals.css'
+import { useState, useEffect } from "react";
+import "../styles/globals.css";
+import {
+  Box,
+  Typography,
+  Grid,
+  Button,
+  TextField,
+  Drawer,
+  SwipeableDrawer,
+  IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
+  if (loaded) {
+    return (
+      <Box >
+        <Navbar />
+        <Component {...pageProps} />
+      </Box>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        border: "1px solid rgba(1,1,1,.3)",
+        background: "rgb(244, 209, 96)",
+        color:'rgba(1,1,1,.8)',
+
+        zIndex: 999,
+        p: { xs:'.5rem', lg:".5rem 2.5rem"},
+        position: "sticky",
+        top: "0",
+        width: "100%",
+      }}
+    >
+      {/* <img src="/logo.png" style={{ objectFit:'contain' , width:'200px' , height:'50px' }} /> */}
+      <Box sx={{  height:'50px' , background:"" , width:'200px' ,  backgroundImage: "url(/logo.png)",
+           backgroundSize: "cover",
+          backgroundPosition: "center", }}>
+      </Box>
+      <IconButton
+        sx={{
+          display: { lg: "none", xs: "flex" },
+        }}
+        onClick={()=>{
+          setOpen(true)
+        }}
+      >
+        <MenuIcon />
+      </IconButton>
+      <SwipeableDrawer open={open}   anchor={"top"}      onClose={()=>{
+          setOpen(false)
+        }} >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "",
+            alignItems: "center",
+            width: "100%",
+            height:'100%',
+            background:'rgb(244, 209, 96)',
+            padding:'2.5rem 0'
+          }}
+        >
+            <Box sx={{  height:'50px' , background:"" , width:'200px' ,  backgroundImage: "url(/logo.png)",
+           backgroundSize: "cover",
+          backgroundPosition: "center", }}>
+      </Box>
+      <Link style={{ width:'100%' }} href="/about">
+          <a style={{ width:'100%' }} >
+            <Typography
+              sx={{ width:'100%', textAlign:'center', '&:hover':{ background:'rgb(29, 93, 155)' } , fontSize: "14px", padding: "1.5rem 0", FontWeight: 3600 }}
+            >
+              {"About"}
+            </Typography>
+          </a>
+        </Link>
+        <Link style={{ width:'100%' }} href="/#services">
+          <a style={{ width:'100%' }} >
+            <Typography
+              sx={{ width:'100%', textAlign:'center', '&:hover':{ background:'rgb(29, 93, 155)' } , fontSize: "14px", padding: "1.5rem 0", FontWeight: 3600 }}
+            >
+              {"Services"}
+            </Typography>
+          </a>
+        </Link>
+
+        <Link style={{ width:'100%' }} href="/gallery">
+          <a style={{ width:'100%' }} >
+            <Typography
+              sx={{ width:'100%', textAlign:'center', '&:hover':{ background:'rgb(29, 93, 155)' } , fontSize: "14px", padding: "1.5rem 0", FontWeight: 3600 }}
+            >
+              {"Gallery"}
+            </Typography>
+          </a>
+        </Link>
+
+        <Link style={{ width:'100%' }} href="/#contact">
+          <a style={{ width:'100%' }} >
+            <Typography
+              sx={{ width:'100%', textAlign:'center', '&:hover':{ background:'rgb(29, 93, 155)' } , fontSize: "14px", padding: "1.5rem 0", FontWeight: 3600 }}
+            >
+              {"Contact"}
+            </Typography>
+          </a>
+        </Link>
+        </Box>
+        
+      </SwipeableDrawer>
+
+      <Box
+        sx={{
+          display: { xs: "none", lg: "flex" },
+          justifyContent: "space-between",
+          alignItems: "center",
+          width: "30%",
+        }}
+      >
+        <Link href="/about">
+          <a>
+            <Typography
+              sx={{ fontSize: "14px", margin: "0", FontWeight: 3600 }}
+            >
+              {"About"}
+            </Typography>
+          </a>
+        </Link>
+        <Link href="/#services">
+          <a>
+            <Typography
+              sx={{ fontSize: "14px", margin: "0", FontWeight: 3600 }}
+            >
+              {"Services"}
+            </Typography>
+          </a>
+        </Link>
+
+        <Link href="/gallery">
+          <a>
+            <Typography
+              sx={{ fontSize: "14px", margin: "0", FontWeight: 3600 }}
+            >
+              {"Gallery"}
+            </Typography>
+          </a>
+        </Link>
+
+        <Link href="/#contact">
+          <a>
+            <Typography
+              sx={{ fontSize: "14px", margin: "0", FontWeight: 3600 }}
+            >
+              {"Contact"}
+            </Typography>
+          </a>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
